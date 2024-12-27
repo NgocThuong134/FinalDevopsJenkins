1b6693e502ac937fd49178fcd1c46fe51f818026
NgocThuong134 <gnouht89@gmail.com>
2024-12-27 14:44:39 +0700

    final devops update api

--- main.go
+++ main.go
@@ -19,7 +19,8 @@ func main() {
     router := gin.Default()
 
 	router.GET("/ping", ping)
-	
+	router.GET("/hello", hello)
+
     router.GET("/items", getItems)
     router.GET("/items/:id", getItemByID)
     router.POST("/items", createItem)
@@ -29,6 +30,9 @@ func main() {
     router.Run(":7080")
 }
 
+func hello(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{"message": "Hello !!!"})
+}
 
 func ping(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{"message": "pong"})
